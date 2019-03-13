3a2905df15ce74045bf26e7e5ab01c94221ff207
Sydney Pollak <sydney.p@me.com>
2019-03-13 15:22:42 +0100

    Sentry.captureException crashes iOS app if not provided an Error object #111

--- src/js/Ionic/SentryIonicErrorHandler.ts
+++ src/js/Ionic/SentryIonicErrorHandler.ts
@@ -2,7 +2,7 @@ class SentryIonicErrorHandler extends IonicErrorHandler {
   handleError(error) {
     super.handleError(error);
     try {
-      captureException(error.originalError || error);
+      captureException(error.stack ? error.originalError : new Error(error.message || error));
     } catch (e) {
       console.error(e);
     }
