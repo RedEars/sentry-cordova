3b0725d494acd333d2a25e8bd5e0b96391ddde25
Sydney Pollak <sydney.p@me.com>
2019-03-13 16:43:44 +0100

    Changes

--- src/js/Ionic/SentryIonicErrorHandler.ts
+++ src/js/Ionic/SentryIonicErrorHandler.ts
@@ -2,7 +2,7 @@ class SentryIonicErrorHandler extends IonicErrorHandler {
   handleError(error) {
     super.handleError(error);
     try {
-      captureException(error.stack ? error.originalError : new Error(error.message || error));
+      captureException(error.stack ? error.originalError : new Error(error.originalError || error));
     } catch (e) {
       console.error(e);
     }
